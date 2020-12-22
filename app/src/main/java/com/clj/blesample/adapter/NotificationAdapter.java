ab69e2222199b1c6ff1b61eec207e9d03e016af1
murali <murali.e@sosaley.in>
2020-12-22 15:15:50 +0530

    Notification Commit

--- app/src/main/java/com/clj/blesample/adapter/NotificationAdapter.java
+++ app/src/main/java/com/clj/blesample/adapter/NotificationAdapter.java
@@ -1,32 +1,73 @@
 package com.clj.blesample.adapter;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.clj.blesample.R;
+import com.clj.blesample.model.NotificationDTO;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.NotificationViewHolder> {
+
+
+    private Context mCtx;
+    private List<NotificationDTO> notificationDTOList;
+
+
+    public NotificationAdapter(Context mCtx, List<NotificationDTO> notificationDTOList) {
+        this.mCtx = mCtx;
+        this.notificationDTOList = notificationDTOList;
+    }
+
+    public void dataSet(List<NotificationDTO> notificationDTOList) {
+        this.notificationDTOList = notificationDTOList;
+        notifyDataSetChanged();
+    }
+
 
     @NonNull
     @Override
     public NotificationViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+
+        LayoutInflater layoutInflater = LayoutInflater.from(mCtx);
+        View view = layoutInflater.inflate(R.layout.layout_notification_adapter, viewGroup, false);
+        return new NotificationViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull NotificationViewHolder notificationViewHolder, int i) {
 
+
+        Picasso.get().load(notificationDTOList.get(i).getNotiImage()).into(notificationViewHolder.notiImg);
+        notificationViewHolder.notiText.setText(notificationDTOList.get(i).getNotiText());
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return notificationDTOList == null ? 0 : notificationDTOList.size();
     }
 
     class NotificationViewHolder extends RecyclerView.ViewHolder {
 
+        ImageView notiImg;
+        TextView notiText;
+
+
         public NotificationViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            notiImg = (ImageView) itemView.findViewById(R.id.notifImage);
+            notiText = (TextView) itemView.findViewById(R.id.notifyText);
+
         }
     }
 
---
+++ app/src/main/java/com/clj/blesample/model/NotificationDTO.java
@@ -0,0 +1,23 @@
+package com.clj.blesample.model;
+
+public class NotificationDTO {
+
+    private String notiImage;
+    private String notiText;
+
+    public String getNotiImage() {
+        return notiImage;
+    }
+
+    public void setNotiImage(String notiImage) {
+        this.notiImage = notiImage;
+    }
+
+    public String getNotiText() {
+        return notiText;
+    }
+
+    public void setNotiText(String notiText) {
+        this.notiText = notiText;
+    }
+}
